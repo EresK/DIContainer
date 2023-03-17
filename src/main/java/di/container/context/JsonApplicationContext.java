c00c53765f5382000bfea96642179a6fb1bea318
EresK <e.kuular@g.nsu.ru>
2023-03-17 21:55:27 +0700

    json appContext realization

---
+++ src/main/java/di/container/context/JsonApplicationContext.java
@@ -0,0 +1,55 @@
+package di.container.context;
+
+import di.container.beans.BeanDefinition;
+import di.container.beans.ListableBean;
+import di.container.beans.factory.BeanFactory;
+import di.container.configuration.Configuration;
+import di.container.configuration.JsonConfiguration;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class JsonApplicationContext implements ApplicationContext {
+    private final BeanFactory beanFactory;
+    private final Configuration configuration;
+    private final ListableBean listableBean;
+    private final Map<Class<?>, Object> classObjectMap = new ConcurrentHashMap<>();
+
+    public JsonApplicationContext(String configurationPath) throws Exception {
+        beanFactory = new BeanFactory(this);
+        configuration = new JsonConfiguration(configurationPath);
+        listableBean = new ListableBean(configuration.getBeanDefinitions());
+        preInitialization();
+    }
+
+    @Override
+    public <T> T getBean(Class<T> requiredType) throws Exception {
+        BeanDefinition bean = listableBean.getBeanByType(requiredType);
+        return (T) getInstanceByBeanDefinition(bean);
+    }
+
+    @Override
+    public Object getBean(String name) throws Exception {
+        BeanDefinition bean = listableBean.getBeanByName(name);
+        return getInstanceByBeanDefinition(bean);
+    }
+
+    private Object getInstanceByBeanDefinition(BeanDefinition bean) throws Exception {
+        if (bean.isSingleton() && classObjectMap.containsKey(bean.getBeanClass()))
+            return classObjectMap.get(bean.getBeanClass());
+
+        Object obj = beanFactory.getInstance(bean);
+
+        if (bean.isSingleton())
+            classObjectMap.put(bean.getBeanClass(), obj);
+
+        return obj;
+    }
+
+    private void preInitialization() throws Exception {
+        for (BeanDefinition bean : listableBean.getBeanList()) {
+            if (bean.isSingleton() && !bean.isLazyInit())
+                getInstanceByBeanDefinition(bean);
+        }
+    }
+}

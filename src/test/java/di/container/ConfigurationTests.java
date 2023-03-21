9f433d6b02dee3ecbdb19ffb4e4e249d50d55ce8
EresK <e.kuular@g.nsu.ru>
2023-03-22 00:14:54 +0700

    add tests for configurations

---
+++ src/test/java/di/container/ConfigurationTests.java
@@ -0,0 +1,41 @@
+package di.container;
+
+import di.container.beans.BeanDefinition;
+import di.container.configuration.JsonConfiguration;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+public class ConfigurationTests {
+
+    @Test
+    public void correctConfiguration() {
+        Assertions.assertDoesNotThrow(() -> {
+            List<BeanDefinition> beans = new JsonConfiguration(
+                    "src/test/resources/classBasedConfig.json").getBeanDefinitions();
+            Assertions.assertEquals(2, beans.size());
+        });
+
+        Assertions.assertDoesNotThrow(() -> {
+            List<BeanDefinition> beans = new JsonConfiguration(
+                    "src/test/resources/interfaceBasedConfig.json").getBeanDefinitions();
+            Assertions.assertEquals(2, beans.size());
+        });
+
+        Assertions.assertDoesNotThrow(() -> {
+            List<BeanDefinition> beans = new JsonConfiguration(
+                    "src/test/resources/cyclicConfig.json").getBeanDefinitions();
+            Assertions.assertEquals(2, beans.size());
+        });
+    }
+
+    @Test
+    public void incorrectConfiguration() {
+        Assertions.assertThrows(IllegalArgumentException.class, () ->
+                new JsonConfiguration("src/test/resources/incorrectSchemaConfig.json").getBeanDefinitions());
+
+        Assertions.assertThrows(IllegalStateException.class, () ->
+                new JsonConfiguration("src/test/resources/incorrectCyclicConfig.json").getBeanDefinitions());
+    }
+}
